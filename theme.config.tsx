61eaa28ced2c3ce018a18fcce3d9d21caa50ca84
marybethrose5 <104355248+marybethrose5@users.noreply.github.com>
2023-05-22 18:59:48 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,13 +5,10 @@ import { useConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 206 246" fill="none">
-        <circle cx="40" cy="40" r="40" fill="currentColor"/>
         <circle cx="40" cy="206" r="40" fill="currentColor"/>
         <circle cx="166" cy="120" r="40" fill="currentColor"/>
       </svg>
       <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
-        Prompt Engineering Guide
       </span>
     </>
   ),
